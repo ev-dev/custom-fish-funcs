acafaa4be8a7a76d4ee3e9b9092da180ea26e302
ev-dev <evan.reed1100@gmail.com>
2017-12-11 04:27:27 -0500

    working on custom them 4

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -11,7 +11,7 @@ function fish_right_prompt
 	set -g __fish_git_prompt_showcolorhints 'yes'
 	
 	set -g __fish_git_prompt_char_dirtystate "$RD_B±$color_reset"
-	set -g __fish_git_prompt_char_cleanstate "$GR$color_reset"
+	set -g __fish_git_prompt_char_cleanstate "$GR$color_reset"
 	set -g __fish_git_prompt_char_stagedstate "$YW_B●$color_reset"
 	set -g __fish_git_prompt_char_stagedstate "$CY_B…$color_reset"
 	set -g __fish_git_prompt_char_stateseparator ' '
