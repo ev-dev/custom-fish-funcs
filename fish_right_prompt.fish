617c6418f4ba80fdab91929912fdb75b464fe959
ev-dev <evan.reed1100@gmail.com>
2017-12-11 07:44:02 -0500

    Included git-radar

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,33 +1,36 @@
 function fish_right_prompt
-	set RD_B (set_color --bold red)
-	set BL_B (set_color --bold blue)
-	set YW_B (set_color --bold yellow)
-	set YW (set_color yellow)
-	set CY_B (set_color --bold cyan)
-	set MG_B (set_color --bold magenta)	
-	set GR_B (set_color --bold green)
-	set GR (set_color green)
-	set N (set_color normal)
-	set B (set_color --bold white)
-
-  set -g __fish_git_prompt_show_informative_status 'yes'
-	set -g __fish_git_prompt_showcolorhints 'yes'
-	
+#	set RD_B (set_color --bold red)
+	# set BL_B (set_color --bold blue)
+	# set YW_B (set_color --bold yellow)
+	# set YW (set_color yellow)
+	# set CY_B (set_color --bold cyan)
+	# set MG_B (set_color --bold magenta)
+	# set GR_B (set_color --bold green)
+	# set GR (set_color green)
+	# set N (set_color normal)
+	# set B (set_color --bold white)
+# 
+  # set -g __fish_git_prompt_show_informative_status 'yes'
+	# set -g __fish_git_prompt_showcolorhints 'yes'
+	# 
 #	set -g __fish_git_prompt_char_dirtystate "$YW_B±$N $RD_B"
-	set -g __fish_git_prompt_char_dirtystate "$B$N $YW±$YW_B"
+#	set -g __fish_git_prompt_char_dirtystate "$B$N $YW±$YW_B"
 #	set -g __fish_git_prompt_char_dirtystate "$RD_B       ±$color_reset"
 #	set -g __fish_git_prompt_char_cleanstate "$GR_B$N "
-	set -g __fish_git_prompt_char_cleanstate "$B$N $GR_B$N"
-	set -g __fish_git_prompt_char_stagedstate "$B$N $YW_B●$N"
+#	set -g __fish_git_prompt_char_cleanstate "$B$N $GR_B$N"
+#	set -g __fish_git_prompt_char_stagedstate "$B$N $YW_B●$N"
 #	set -g __fish_git_prompt_char_stagedstate "$CY_B…$N"
-	set -g __fish_git_prompt_char_stagedstate "$CY_B...$N"
-	set -g __fish_git_prompt_char_stateseparator ' '
-	
-	set -g __fish_git_prompt_color_branch "magenta"
-
-	set git (__fish_git_prompt "%s ")
+	# set -g __fish_git_prompt_char_stagedstate "$CY_B...$N"
+	# set -g __fish_git_prompt_char_stateseparator ' '
+	# 
+	# set -g __fish_git_prompt_color_branch "magenta"
+# 
+	# set git (__fish_git_prompt "%s ")
 #	set git (__fish_git_prompt "$N             $color_reset%s")
 
-  echo -ns $git
-
+#  echo -ns $git
+#  echo -n (git-rader --fish --fetch)
+#  set_color $fish_color_cwd
+#  git-radar --fish --fetch
+#  set_color normal
 end
---
+++ update.fish
@@ -0,0 +1,1 @@
+/Users/admin/.config/fisherman/update/functions/update.fish
