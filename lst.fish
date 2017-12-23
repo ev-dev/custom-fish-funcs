7d1f4f6a7e685eb5920b427ba56ec5527079acd5
ev-dev <evan.reed1100@gmail.com>
2017-12-23 15:21:54 -0500

    Updates to lst function

--- lst.fish
+++ lst.fish
@@ -1,4 +1,18 @@
-# Defined in /Users/admin/.config/fish/functions/lst.fish @ line 2
+# Defined in - @ line 2
 function lst
-	tree -CD --du -h -L 3 -I node_modules --dirsfirst --filelimit 15 $argv
+	#tree -CL 3 -I node_modules --dirsfirst --filelimit 15 $argv
+    set argCount (count $argv)
+    if test $argCount -gt 1
+        if [ $argv[1] = '-v' ]
+            tree -CD --du -h --dirsfirst -I node_modules -L 3 --filelimit 15 $argv[2]
+        else
+            eko -nltb $RD'- Error use either -v option for verbose or dirname'
+        end
+    else
+        if [ $argv = '-v' ]
+            tree -CD --du -h --dirsfirst -I node_modules -L 3 --filelimit 15
+        else
+            tree -C --dirsfirst -I node_modules -L 3 --filelimit 15 $argv
+        end
+    end
 end
