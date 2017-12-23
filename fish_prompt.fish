1436edcffb23c3da71b5aa95be4d914f2cbec4ad
ev-dev <evan.reed1100@gmail.com>
2017-12-23 15:08:36 -0500

    Updates to fish_prompt and right_prompt including faster prompt time and dimmed command time in right prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,39 +1,28 @@
 function fish_prompt
-  set -l cblue (set_color 69D2E7)
-  set -l cnormal (set_color normal)
-  set -l pwd $cblue(prompt_pwd)$cnormal
-  set -l OS (uname)
-  set -l git_output
-  set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  set -l pwd (set_color 69D2E7)(prompt_pwd)(set_color normal)
   set -g __fish_prompt_char "❯"
-
-  if test $OS = "Linux"
-    alias ls "ls --color=auto"
-  else if test $OS = "Darwin"
-    alias ls "ls -G"
-  end
-
+  set -l ve ""
+  set -l git_output
+  
   if type -q git
-    set normal (set_color normal)
-    set dirty (set_color black -b F38630)
-    set clean (set_color black -b 7AB317)
     set detached (set_color black -b FF0000)
+    
     if set branch_name (git_branch_name)
       set -l gitporcelain (git_porcelain)
       set -l git_distance (git_distance)
+      set -l repo_status
+      set -l branch_color ""
+      
       if test ! -z "$git_distance"
         set git_distance "($git_distance)"
       end
-      set -l repo_status
-      set -l branch_color ""
 
       if git_is_dirty
-        set branch_color "$dirty"
+        set branch_color (set_color black -b FF6524 -o) #orange
       else
-        set branch_color "$clean"
+        set branch_color (set_color black -b 50AF08 -o) #green
       end
-      
-      
+
       if git_is_staged
         if git_is_dirty
           set git_status "± "
@@ -50,17 +39,13 @@ function fish_prompt
         set repo_status ".."
       end
 
-      set git_output "$branch_color $branch_name$repo_status$git_distance $normal $gitporcelain"
+      set git_output "$branch_color $branch_name$repo_status$git_distance $N $gitporcelain"
     end
   end
-
-  set -l ve ""
+  
   if set -q VIRTUAL_ENV
     set ve (printf "(%s)" (basename $VIRTUAL_ENV))
-end
-
-  echo -n -s \
-  "$ve $pwd "\
-  "$git_output"
-  printf "\n%s " "$__fish_prompt_char"
+  end
+  
+  echo -nes "$ve $I$pwd $N$git_output\n$__fish_prompt_char "
 end
--- ls.fish
+++
@@ -1,1 +0,0 @@
-/Users/admin/.config/fisherman/grc/functions/ls.fish
