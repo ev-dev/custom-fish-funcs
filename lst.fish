273969934a26511a31b902f476931c6834681806
ev-dev <evan.reed1100@gmail.com>
2017-12-23 15:26:07 -0500

    Updates to lst function again

--- lst.fish
+++ lst.fish
@@ -1,18 +1,21 @@
 # Defined in - @ line 2
 function lst
-	#tree -CL 3 -I node_modules --dirsfirst --filelimit 15 $argv
-    set argCount (count $argv)
-    if test $argCount -gt 1
-        if [ $argv[1] = '-v' ]
-            tree -CD --du -h --dirsfirst -I node_modules -L 3 --filelimit 15 $argv[2]
+	if test -n "$argv"
+        set argCount (count $argv)
+        if test $argCount -gt 1
+            if [ $argv[1] = '-v' ]
+                tree -CD --du -h --dirsfirst -I node_modules -L 3 --filelimit 15 $argv[2]
+            else
+                eko -nltb $RD'- Error use either -v option for verbose or dirname'
+            end
         else
-            eko -nltb $RD'- Error use either -v option for verbose or dirname'
+            if [ $argv = '-v' ]
+                tree -CD --du -h --dirsfirst -I node_modules -L 3 --filelimit 15
+            else
+                tree -C --dirsfirst -I node_modules -L 3 --filelimit 15 $argv
+            end
         end
     else
-        if [ $argv = '-v' ]
-            tree -CD --du -h --dirsfirst -I node_modules -L 3 --filelimit 15
-        else
-            tree -C --dirsfirst -I node_modules -L 3 --filelimit 15 $argv
-        end
+        tree -C --dirsfirst -I node_modules -L 3 --filelimit 15
     end
 end
