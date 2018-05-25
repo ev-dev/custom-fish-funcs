28d0beaa469e292d719ec420f53824d5dddd885a
Visual-Knowledge <evan.reed1100@gmail.com>
2018-05-25 00:39:43 -0400

    update dl

--- dl.fish
+++ dl.fish
@@ -1,5 +1,25 @@
 # Defined in - @ line 2
-function dl --description a --argument url file and perform some action
-	echo "Downloading from $url"
-    echo "Performing $action"
+function dl
+	set action "$argv[2]"
+    set file (string split / "$argv[1]")[-1]
+
+    proc -b "Downloading "$YL$I"$file"$N
+
+    spin "wget -q $argv[1] ^/dev/null"
+    suc -a 'Download complete.'
+
+    if [ "$action" = '-x' ]
+        set exDir (string replace -r '\.tar\.gz$' '' "$file")
+
+        proc 'Extracting tarball'
+        spin "tar -zxf $file"
+
+        if test -d "./$exDir"
+            suc -a 'Extraction complete.'
+            cd $exDir
+            exa
+        else
+            warn -a "Extraction finished but no directory '$exDir' was found"
+        end
+    end
 end
