9e9c20c101d7ef219edb596a520ec1be4359a586
Visual-Knowledge <evan.reed1100@gmail.com>
2018-06-21 10:50:46 -0400

    Fixed iterm2 integration BS. overwrote prompt

--- iterm2_fish_prompt.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function iterm2_fish_prompt
-	printf ">"
-end
---
+++ iterm2_print_user_vars.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function iterm2_print_user_vars
+	iterm2_set_user_var MY_VAR 'hello world'
+end
